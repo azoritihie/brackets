06045bd72d685a8b8a79152662b27efa0e8a8e03
azoritihie <butterfly140611@gmail.com>
2021-03-10 22:11:33 +0400

    init: impl check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let stack = [];
+  let openChar = [];
+  let closeChar = [];
+  let brackets = str.split('');
+    
+  //Insert paired characters into different arrays
+  for( let i = 0; i < bracketsConfig.length; i++) {
+    openChar.push(bracketsConfig[i][0]);
+    closeChar.push(bracketsConfig[i][1]);
+  }
+  
+  for(let i=0; i < brackets.length; i++){
+    if(openChar.includes(brackets[i])){
+      if (closeChar.indexOf(brackets[i])===stack[stack.length-1]) {
+        stack.pop()
+      } else stack.push(openChar.indexOf(brackets[i]));
+    }
+    else if (closeChar.includes(brackets[i])){
+      if(closeChar.indexOf(brackets[i]) === stack[stack.length-1]){
+        stack.pop();
+      } else return false;
+    }
+  }
+  return stack.length == 0; 
 }
